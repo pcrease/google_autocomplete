391bffa4ff2859a0575e714468b4da7d65a7595c
Paul Crease <paul.e.crease@gmail.com>
2014-02-06 15:35:51 +0100

    changes made to get nationaliities

--- org.google_autocomplete/main/autocomplete_search.py
+++ org.google_autocomplete/main/autocomplete_search.py
@@ -1,20 +1,27 @@
 # Country ISO codes: http://en.wikipedia.org/wiki/ISO_3166-1_alpha-3
 # 2013-10-17/18
-# Ralph Straumann, ralphstraumann.ch
+# Based on a project by Ralph Straumann, ralphstraumann.ch
 
 import urllib, urllib2
 import json
 import time
 import codecs
+import os
+from nltk.corpus import stopwords
 
-in_file = 'google_autocomplete/data_sources/countries.txt'
+
+dir = os.path.split(os.path.dirname(__file__))
+
+in_file = os.path.join(dir[0],'data_sources/nationality_list.txt')
+
 sep = ','
 iso_field = 'iso_a3'
 name_field = 'admin'
 plural_field = 'plural'
 definite_field = 'definite'
+nationality_field='nationalities'
 
-out_file = 'country_stereotypes_results.txt'
+out_file = os.path.join(dir[0], 'data_sources/country_stereotypes_results.txt')
 
 # API endpoint
 google_endpoint = 'http://google.com/complete/search?output=firefox&q='
@@ -49,6 +56,14 @@ def build_phrase(name, plural, definite):
 
     return u'%s %s so ' % (phrase, name)
 
+def build_nationality_phrase(nationality):
+    ''' Given a country name and <plural>, <definite> flag, this function 
+    assembles the search query '''
+
+    phrase = 'why are'
+    print u'%s %s so ' % (phrase, nationality)
+    return u'%s %s so ' % (phrase, nationality)
+
 
 def query_google(phrase):
     ''' Query using the Google endpoint '''
@@ -65,6 +80,7 @@ iso_idx = find_index(iso_field, in_file)
 name_idx = find_index(name_field, in_file)
 plural_idx = find_index(plural_field, in_file)
 definite_idx = find_index(definite_field, in_file)
+nationality_idx = find_index(nationality_field,in_file)
 
 with codecs.open(in_file, 'r', 'utf-8') as f:
 
@@ -81,15 +97,21 @@ with codecs.open(in_file, 'r', 'utf-8') as f:
             name = items[name_idx]
             plural = items[plural_idx]
             definite = items[definite_idx]
-            
-            phrase = build_phrase(name, plural, definite)
+            nationality=items[nationality_idx]
+            print stopwords.words('english')
+                        
+            phrase = build_nationality_phrase(nationality)
             try:
                 results = query_google(phrase)
+                
+                
                 if len(results) > 0:
                     for result in results:
-                        f_out.write('%s\t%s\t%s\n' % (iso, name, result))
+                        filtered_words = [w for w in result if not w in stopwords.words('english')]
+                        
+                        f_out.write('%s\t%s\t%s\n' % (iso, nationality, result))
                 else:
-                    f_out.write('%s\t%s\t\n' % (iso, name))
+                    f_out.write('%s\t%s\t\n' % (iso, nationality))
             except:
-                f_out.write('%s\t%s\t\n' % (iso, name))
+                f_out.write('%s\t%s\t\n' % (iso, nationality))
 
